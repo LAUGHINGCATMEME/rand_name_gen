5e01ca811c8130d3fb73d8d18777e884bf92d908
Alright Yagami <77753224+LAUGHINGCATMEME@users.noreply.github.com>
2022-09-09 01:29:44 +0530

    Create Version_2.cxx

---
+++ Version_2.cxx
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+
+string gen_random(const int len) {
+    static const char alphanum[] = "abcdefghijklmnopqrstuvwxyz";
+    string tmp_s;
+    tmp_s.reserve(len);
+    for (int i = 0; i < len; ++i) {
+        tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
+    }    
+    return tmp_s;
+}        
+
+
+int main(){
+    while (true){
+        // Variables
+        string word, rand_str;
+        clock_t start, end;
+        int no_of_tries = 0;
+        cout<<"8=====D ";
+        cin>>word;
+        
+        // Loop
+        time(&start);
+        do{
+            no_of_tries++;
+            rand_str = gen_random(size(word));
+            //cout<<rand_str<<endl;
+        }while(rand_str != word);
+        time(&end);
+        
+        // Output
+        cout<<no_of_tries<<endl;
+        cout<<"Time = "<<(end-start)<<" secs"<<endl;
+    }
+    return 0;
+}
